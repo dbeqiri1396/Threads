b40ec154661aba4bac7f1135af88bace02f6b099
dbeqiri1396 <147188905+dbeqiri1396@users.noreply.github.com>
2024-10-24 15:41:32 -0400

    Revert "Latest Updates"
    This reverts commit 03adff888f69ac7b38c49da68a65b9f2a17db842.

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,45 +1,33 @@
-"use client";
-
-import Image from "next/image";
+'use client';
+import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
-import { sidebarLinks } from "@/constants";
+function BottomBar() {
+    const pathname = usePathname();
+    return (
+        <section className="bottombar">
+            <div className="bottombar_container">
+                {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length >1) || pathname === link.route;
+                    return(
+                    <Link
+                        href={link.route}
+                        key={link.label}
+                        className={`lefsidebar_link ${isActive && 'bg-primary-500'}`}>
+                        <Image
+                            src={link.imgURL} 
+                            alt={link.label}
+                            width={24}
+                            height={24}/>
+                        <p className="text-subtle-medium-1 text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+                    </Link>
+                )})}
 
-function Bottombar() {
-  const pathname = usePathname();
-
-  return (
-    <section className='bottombar'>
-      <div className='bottombar_container'>
-        {sidebarLinks.map((link) => {
-          const isActive =
-            (pathname.includes(link.route) && link.route.length > 1) ||
-            pathname === link.route;
-
-          return (
-            <Link
-              href={link.route}
-              key={link.label}
-              className={`bottombar_link ${isActive && "bg-primary-500"}`}
-            >
-              <Image
-                src={link.imgURL}
-                alt={link.label}
-                width={16}
-                height={16}
-                className='object-contain'
-              />
-
-              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
-                {link.label.split(/\s+/)[0]}
-              </p>
-            </Link>
-          );
-        })}
-      </div>
-    </section>
-  );
+            </div>
+        </section>
+    )
 }
 
-export default Bottombar;
+export default BottomBar;
